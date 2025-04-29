34c0f643c0dcc6ecc072d6b279ed362efc7fe04c
NickNinjaGit <balguing@gmail.com>
2025-04-28 23:19:48 -0300

    fixing definitive backend

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,49 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './routes/Auth.js';
+import express from "express";
+import conn from "./db/conn.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
-dotenv.config();
+// models
+import { User } from "./models/User.js";
+import { Post } from "./models/Post.js";
+import { Comment } from "./models/Comment.js";
+import { Course } from "./models/Course.js";
+import { Module } from "./models/Module.js";
+import { Lesson } from "./models/Lesson.js";
+
+// Routes
+import { UserRoutes } from "./routes/UserRoutes.js";
+// import postRoutes from "./routes/postRoutes.js";
+// import courseRoutes from "./routes/courseRoutes.js";
 
 const app = express();
-
-app.use(cors());
+// Config JSON response
 app.use(express.json());
+// Config cookies
+app.use(cookieParser());
+// config body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// Solve CORS
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
-app.use('/api', authRoutes);
+try {
+  await conn.authenticate();
+  await conn.sync({ force: false });
+  console.log("Database connected");
+} catch (error) {
+  console.log(error);
+}
 
+// Routes
+app.use("/users", UserRoutes);
+// app.use("/post", postRoutes);
+// app.use("/course", courseRoutes);
 
-// Para o servidor nao cair -> se ficar inativo ele desliga, entao o bot fica chamando o ping 
-app.get('/api/ping', (req, res) => {
-    res.json({ status: 'pong' });
-  });
+// Public folder for images
+app.use(express.static("public"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
